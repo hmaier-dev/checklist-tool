edcd6c74c7ec92c7f582502316c63ecbeef211dc
Mensch <hendrik_maier@protonmail.com>
2025-02-23 17:02:25 +0100

    feat(handlers): receive the form values and reload the page

--- internal/server/server.go
+++ internal/server/server.go
@@ -16,7 +16,7 @@ func NewServer() *Server {
 	router.HandleFunc("/checklist", handlers.CheckList).Methods("GET")
   sub := router.PathPrefix("/checklist").Subrouter()
   sub.HandleFunc("/new", handlers.Formular).Methods("GET") // Show the New-formular
-  sub.HandleFunc("/new", handlers.).Methods("POST") // Add new Checklist
+  sub.HandleFunc("/new", handlers.NewEntry).Methods("POST") // Add new Checklist
   sub.HandleFunc(`/{id:[0-9]{15}}`, handlers.Display).Methods("GET")
   sub.HandleFunc(`/update-{id:\d{15}}`, handlers.Update).Methods("POST")
 
