1d45616cec9bb099fff304344b5617591d228822
Mensch <hendrik_maier@protonmail.com>
2025-02-23 16:52:48 +0100

    feat(html): display the formular for new checklist

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,7 +15,8 @@ func NewServer() *Server {
 	router.HandleFunc("/health", handlers.HealthCheck).Methods("GET")
 	router.HandleFunc("/checklist", handlers.CheckList).Methods("GET")
   sub := router.PathPrefix("/checklist").Subrouter()
-  sub.HandleFunc("/new", handlers.New).Methods("GET")
+  sub.HandleFunc("/new", handlers.Formular).Methods("GET") // Show the New-formular
+  sub.HandleFunc("/new", handlers.).Methods("POST") // Add new Checklist
   sub.HandleFunc(`/{id:[0-9]{15}}`, handlers.Display).Methods("GET")
   sub.HandleFunc(`/update-{id:\d{15}}`, handlers.Update).Methods("POST")
 
