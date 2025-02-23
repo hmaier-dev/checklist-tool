69869fc0257aec0ebc2b76a88c476a67ae5269ff
Mensch <hendrik_maier@protonmail.com>
2025-02-23 14:38:04 +0100

    feat(server): add basic setup for http-server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,21 @@
 package server
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/gorilla/mux"
+  "github.com/hmaier-dev/checklist-tool/internal/handlers"
+)
 
 func Hello(){
   fmt.Println("Hello from Server!")
+}
+
+type Server struct {
+	Router *mux.Router
+}
+
+func NewServer() *Server {
+	router := mux.NewRouter()
+	router.HandleFunc("/health", handlers.HealthCheck).Methods("GET")
+	return &Server{Router: router}
 }
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+  "log"
+  "net/http"
   "github.com/hmaier-dev/checklist-tool/internal/server"
 )
 
 func main() {
   server.Hello()
+
+  srv := server.NewServer()
+	log.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", srv.Router))
 }
