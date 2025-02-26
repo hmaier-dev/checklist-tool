6cbdffcf0e353016495d0629a1802fdcea81c8c6
maierh-abrm <.>
2025-02-26 08:51:20 +0100

    feat(pdf): add http-endpoint for post-request to generate pdf

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,10 +15,15 @@ func NewServer() *Server {
 	router := mux.NewRouter()
 	router.HandleFunc("/checklist", handlers.Home).Methods("GET")
   sub := router.PathPrefix("/checklist").Subrouter()
-  sub.HandleFunc("/new", handlers.NewEntry).Methods("POST")
+  // GET
   sub.HandleFunc("/blanko", handlers.DisplayBlanko).Methods("Get")
   sub.HandleFunc(`/{id:[0-9]{15}}`, handlers.Display).Methods("GET")
+  
+  // POST
+  sub.HandleFunc("/new", handlers.NewEntry).Methods("POST")
   sub.HandleFunc(`/update_{id:[0-9]{15}}`, handlers.Update).Methods("POST")
+  sub.HandleFunc(`/generate_{id:[0-9]{15}}`, handlers.GeneratePDF).Methods("POST")
+
   router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 
