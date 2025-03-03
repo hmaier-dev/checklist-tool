f7e91a686fa03b98c8475fafea3488610c9c5fef
Mensch <hendrik_maier@protonmail.com>
2025-03-03 19:28:31 +0100

    feat(url): replace imei url for unique combination form datetime and imei

--- internal/structs/structs.go
+++ internal/structs/structs.go
@@ -1,28 +1,30 @@
 package structs
 
-// To received data given in the new-formular
+// Represents data needed to create a new database entry
 type FormularData struct {
-	IMEI     string
-	ITA      string
-	Name     string
-	Ticket   string
-	Model    string
+	IMEI   string
+	ITA    string
+	Name   string
+	Ticket string
+	Model  string
+	Path   string
 }
 
 // Represents a single checklist
 type ChecklistEntry struct {
-	IMEI     string `yaml:"imei"`
-	ITA      string `yaml:"ita"`
-	Name     string `yaml:"name"`
-	Ticket   string `yaml:"ticket"`
-	Model    string `yaml:"model"`
-	Yaml     string `yaml:"yaml"`
+	IMEI   string `yaml:"imei"`
+	ITA    string `yaml:"ita"`
+	Name   string `yaml:"name"`
+	Ticket string `yaml:"ticket"`
+	Model  string `yaml:"model"`
+	Path   string `yaml:"path"`
+	Yaml   string `yaml:"yaml"`
 }
 
 // Single checkpoint of the list
 type ChecklistItem struct {
-	Task     string           `json:"task"`
-	Checked  bool             `json:"checked"`
-	Children []*ChecklistItem `json:"children,omitempty"`
-	IMEI     string           `json:"imei"`
+	Task     string           `yaml:"task"`
+	Checked  bool             `yaml:"checked"`
+	Children []*ChecklistItem `yaml:"children,omitempty"`
+	Path     string           `yaml:"Path"`
 }
