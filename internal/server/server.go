87fa41b30fc284789ed10c1d4077d758f93e1d62
hmaier-dev <73271847+hmaier-dev@users.noreply.github.com>
2025-04-16 10:52:07 +0200

    feat(html)!: add navbar generated from the routes
    * feat(html): add nav template
    * feat(html): add template to home.html
    * feat: load nav template in Home-Handler
    * feat: add new struct for storing NavItems
    * feat: populate NavList by Walking the Router
    * feat: Populate the NavList
    * feat: add NavList to template
    * feat(nav): make the nav appear on reset and delete

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,13 @@
 package server
 
 import (
-  "github.com/gorilla/mux"
-  "github.com/hmaier-dev/checklist-tool/internal/handlers"
-  "net/http"
+	"net/http"
+	"regexp"
+	"log"
+
+	"github.com/gorilla/mux"
+	"github.com/hmaier-dev/checklist-tool/internal/handlers"
+	"github.com/hmaier-dev/checklist-tool/internal/structs"
 )
 
 
@@ -16,11 +20,11 @@ func NewServer() *Server {
 	router.HandleFunc("/checklist", handlers.Home).Methods("GET")
   sub := router.PathPrefix("/checklist").Subrouter()
   // GET
-  sub.HandleFunc("/blanko", handlers.DisplayBlanko).Methods("Get")
   sub.HandleFunc("/delete", handlers.DisplayDelete).Methods("Get")
   sub.HandleFunc("/reset", handlers.DisplayReset).Methods("Get")
   sub.HandleFunc(`/{id:\d{14}_\d{15}}`, handlers.Display).Methods("GET")
   sub.HandleFunc(`/download_{id:\d{14}_\d{15}}`, handlers.GeneratePDF).Methods("GET")
+  sub.HandleFunc("/blanko", handlers.DisplayBlanko).Methods("Get")
   
   // POST
   sub.HandleFunc("/new", handlers.NewEntry).Methods("POST")
@@ -30,7 +34,35 @@ func NewServer() *Server {
   sub.HandleFunc(`/reset_{id:\d{14}_\d{15}}`, handlers.ResetChecklistForEntry).Methods("POST")
 
   router.PathPrefix("/checklist/static/").Handler(http.StripPrefix("/checklist/static/", http.FileServer(http.Dir("./static/"))))
-
+	
+	IndexRoute(router)
 
 	return &Server{Router: router}
+}
+
+// Populates the NavList
+func IndexRoute(router *mux.Router){
+	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		path, _ := route.GetPathTemplate()
+    method, _ := route.GetMethods()
+		
+		// Exclude routes which do not display anything
+		r, err := regexp.Compile("id")
+		if err != nil {
+			log.Println("Error compiling regex:", err)
+			return err
+		}
+		match := r.MatchString(path)
+
+		if len(method) > 0 && method[0] == "GET" && match != true{
+			entry := structs.NavItem{
+				Name: path,
+				Path: path,
+			}
+			handlers.NavList = append(handlers.NavList, entry)
+
+		}
+    return nil
+	})
+
 }
