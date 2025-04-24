70afaea35addf34b8e72c683ca7befeecc233136
Work <me@work.de>
2025-04-24 09:34:22 +0200

    feat(nav): remove IndexRoute(), declare nav manually in handlers.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,12 +2,8 @@ package server
 
 import (
 	"net/http"
-	"regexp"
-	"log"
-
 	"github.com/gorilla/mux"
 	"github.com/hmaier-dev/checklist-tool/internal/handlers"
-	"github.com/hmaier-dev/checklist-tool/internal/structs"
 )
 
 
@@ -36,35 +32,6 @@ func NewServer() *Server {
   sub.HandleFunc("/upload", handlers.ReceiveUpload).Methods("POST")
 
   router.PathPrefix("/checklist/static/").Handler(http.StripPrefix("/checklist/static/", http.FileServer(http.Dir("./static/"))))
-	
-	IndexRoute(router)
 
 	return &Server{Router: router}
-}
-
-// Populates the NavList
-func IndexRoute(router *mux.Router){
-	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-		path, _ := route.GetPathTemplate()
-    method, _ := route.GetMethods()
-		
-		// Exclude routes which do not display anything
-		r, err := regexp.Compile("id")
-		if err != nil {
-			log.Println("Error compiling regex:", err)
-			return err
-		}
-		match := r.MatchString(path)
-
-		if len(method) > 0 && method[0] == "GET" && match != true{
-			entry := structs.NavItem{
-				Name: path,
-				Path: path,
-			}
-			handlers.NavList = append(handlers.NavList, entry)
-
-		}
-    return nil
-	})
-
 }
